ad41f0f3e8a7275da6e2ff2cf60a547daf602595
Chaker Zaghbani <czaghbani@transtex-llc.com>
2020-12-01 17:44:19 -0500

    First deploy

---
+++ SpeedLimitZoneApp/Controllers/AccountController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using SpeedLimitZoneApp.Models;
+using SpeedLimitZoneApp.App_Start;
+
+namespace SpeedLimitZoneApp.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private CognitoUserManager _signInManager;
+
+        public AccountController()
+        {
+        }      
+
+        //
+        // GET: /Account/Login
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        //
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            CognitoUserManager _signInManager = CognitoUserManager.CognitoManager;
+            CognitoUser cognitoUser = new CognitoUser() { UserName = model.Email };// "portail-client@transtex-llc.com" };
+            var isvalid = await _signInManager.CheckPasswordAsync(cognitoUser, model.Password);// "crig7wee*uy7TANT");
+            if (isvalid)
+            {
+                Session["IdToken"] = cognitoUser.IdToken;
+                SignInAsync(cognitoUser, true);
+                return RedirectToLocal(returnUrl);
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid login attempt.");
+                return View(model);
+            }
+        }
+
+       
+
+        //
+        // POST: /Account/LogOff
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            return RedirectToAction("Index", "Home");
+        }
+
+       
+        #region Helpers
+        private IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+        
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        private void SignInAsync(CognitoUser user, bool isPersistent)
+        {
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            ClaimsIdentity identity = new ClaimsIdentity(DefaultAuthenticationTypes.ApplicationCookie);
+            identity.AddClaim(new Claim(ClaimTypes.Name, user.UserName));
+            AuthenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = isPersistent }, identity);
+        }
+        #endregion
+    }
+}
